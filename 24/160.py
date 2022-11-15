7f186dea796b7e540f29657fa6c06dd985f84fd0
wBlackPrince <eduard.nikitin.04@list.ru>
2022-11-15 21:48:16 +0300

    24_161_added

--- 24/160.py
+++ 24/160.py
@@ -18,33 +18,33 @@ def finder(a,flag = False,letter = "?"):
         return dc[letter]
 
     s_dc = sorted(dc.items(),key = lambda x: x[1])
-    mini = min([i for i in dc.values() if i != 0])
+    maxi = max(dc.values())
     # проверка
-    s_dc = list(filter(lambda x: x[1] == mini,s_dc))
-    return sorted(s_dc,key = lambda x: x[0])[0]
+    s_dc = list(filter(lambda x: x[1] == maxi,s_dc))
+    return sorted(s_dc,key = lambda x: x[0])[-1]
 
 
 
 # поиск строки с самым малым количеством символов
 arr = []
-maxi = 0
+mini = 10*4
 for i in a:
-    c = i.count("Q")
+    c = i.count("A")
     # должна быть хотя бы 1 A
     if c == 0:
         continue
-    if c > maxi:
-        maxi = c
+    if c < mini:
+        mini = c
         arr.clear()
         arr.append(i)
-    elif c == maxi:
+    elif c == mini:
         arr.append(i)
 
-s = arr[-1]
+s = arr[0]
 # заветная буква
 letter = finder(s)[0]
 print(letter)
 print(finder(a_full,flag=True,letter=letter))
 
 # ответ
-#C38412
+#V38429
