73af2a97396b398a82920127ba88e1db4ddb51dd
wBlackPrince <eduard.nikitin.04@list.ru>
2022-11-15 23:48:00 +0300

    24-220

--- 24/220.py
+++ 24/220.py
@@ -1,6 +1,17 @@
 with open("24data/24-215.txt") as f:
     a = f.read()
 
+s = ""
+for i in range(len(a)):
+    if i < 2 or i > len(a)-2:
+        s += a[i]
+        continue
+    elif a[i-1] in "123" and a[i] in "ABC" and a[i+1] in "123" and a[i-2] in "ABC" and a[i+2] in "ABC":
+        s += a[i]*3
+    else:
+        s += a[i]
+
+a = s
 c = 0
 count = 0
 maxi = 0
